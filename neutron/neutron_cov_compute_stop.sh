8d2efaf93888addbf0b57ac1e48238e9b054d0b9
Aleksey Galkin <leks.molecul@gmail.com>
2015-04-22 00:16:36 +0300

    Clear old data

--- neutron/neutron_cov_compute_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Kill parent process for Neutron-Service"
-kill `ps hf -C coverage | grep "neutron-openvswitch-agent" |awk '{ print $1; exit }'`
--- neutron/neutron_cov_controller_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Kill parent process for Neutron-Service"
-kill `ps hf -C coverage | grep "neutron-server" |awk '{ print $1; exit }'`
-kill `ps hf -C coverage | grep "neutron-openvswitch-agent" |awk '{ print $1; exit }'`
-kill `ps hf -C coverage | grep "neutron-dhcp-agent" |awk '{ print $1; exit }'`
-kill `ps hf -C coverage | grep "neutron-l3-agent" |awk '{ print $1; exit }'`
-kill `ps hf -C coverage | grep "neutron-metadata-agent" |awk '{ print $1; exit }'`
